fe51c717df33016f96d0eaa1f29d3ecaa5a386c8
chandramaloo <maloochandra@gmail.com>
2020-10-11 21:25:49 +0530

    inital upload

---
+++ js/index.js
@@ -0,0 +1,135 @@
+var a=-1;
+
+$(document).ready(function(){
+    $("#b0").click(function(){
+	$(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 0){
+	    $("#b0").addClass("cont-box-active");
+	    $("#t0").addClass("cont-text-active");
+	    a = 0;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b1").click(function(){
+    $(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 1){
+	    $("#b1").addClass("cont-box-active");
+	    $("#t1").addClass("cont-text-active");
+	    a = 1;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b2").click(function(){
+	$(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 2){
+	    $("#b2").addClass("cont-box-active");
+	    $("#t2").addClass("cont-text-active");
+	    a = 2;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b3").click(function(){
+    $(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 3){
+	    $("#b3").addClass("cont-box-active");
+	    $("#t3").addClass("cont-text-active");
+	    a = 3;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b4").click(function(){
+	$(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 4){
+	    $("#b4").addClass("cont-box-active");
+	    $("#t4").addClass("cont-text-active");
+	    a = 4;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b5").click(function(){
+    $(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 5){
+	    $("#b5").addClass("cont-box-active");
+	    $("#t5").addClass("cont-text-active");
+	    a = 5;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b6").click(function(){
+	$(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 6){
+	    $("#b6").addClass("cont-box-active");
+	    $("#t6").addClass("cont-text-active");
+	    a = 6;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b7").click(function(){
+	$(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    	if(a !== 7){
+	    $("#b7").addClass("cont-box-active");
+	    $("#t7").addClass("cont-text-active");
+	    a = 7;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b8").click(function(){
+	$(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    if(a !== 8){
+	    $("#b8").addClass("cont-box-active");
+	    $("#t8").addClass("cont-text-active");
+	    a = 8;
+	} else {
+	    a= -1;
+	}
+    });
+
+    $("#b9").click(function(){
+	$(".cont-box").removeClass("cont-box-active");
+	$(".cont-text").removeClass("cont-text-active");
+    if(a !== 9){
+	    $("#b9").addClass("cont-box-active");
+	    $("#t9").addClass("cont-text-active");
+	    a = 9;
+	} else {
+	    a= -1;
+	}
+    });
+});
+
+$(document).ready(function(){
+	var element = document.getElementById('frame')
+	$('element').attr('data','pages/home.html');
+});
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-65529195-1', 'auto');
+  ga('send', 'pageview');
